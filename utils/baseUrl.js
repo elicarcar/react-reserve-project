dabcbc5ac229c7f2912a4fa81894ad9cd1ec88a2
elicarcar <elisabetta.carcar@gmail.com>
2020-01-18 22:54:45 +0100

    changed the prod env

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://react-reserve-eliC.now.sh"
+    ? "https://react-reserve-eliC.herokuapp.sh"
     : "http://localhost:3000";
 
 export default baseUrl;
